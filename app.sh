4be1ec90d11e5141386e63465c7adb9ee376991c
hlong24082k <hlong.24082k@gmail.com>
2022-12-13 11:45:30 +0700

    Fix command line in app file

--- app.sh
+++ app.sh
@@ -19,7 +19,7 @@ fi
 # Create a configuration file
 touch sonar-project.properties
 
-echo -e "sonar.projectKey=$name \nisonar.projectName=$name \nsonar.projectVersion=1.0 \nsonar.sources=. \nsonar.sourceEncoding=UTF-8" > sonar-project.properties
+echo -e "sonar.projectKey=$name \nsonar.projectName=$name \nsonar.projectVersion=1.0 \nsonar.sources=. \nsonar.sourceEncoding=UTF-8" > sonar-project.properties
 
 # Connect to the Server
 docker run --rm --link sonarqube -e SONAR_HOST_URL="http://192.168.64.13:9000" -e SONAR_LOGIN="$token" -v ${PWD}:/usr/src sonarsource/sonar-scanner-cli
